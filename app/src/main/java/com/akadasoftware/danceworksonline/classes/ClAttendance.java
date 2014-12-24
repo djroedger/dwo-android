1212ceb0407ce24f8965810ef3881c16645f975a
ksudu94 <ks281923>
2014-12-24 00:02:22 -0500

    Github problems... random comments added to test commit

--- app/src/main/java/com/akadasoftware/danceworksonline/classes/ClAttendance.java
+++
@@ -1,7 +0,0 @@
-package com.akadasoftware.danceworksonline.classes;
-
-/**
- * Created by Kyle on 7/21/2014.
- */
-public class ClAttendance {
-}
--- app/src/main/java/com/akadasoftware/danceworksonline/classes/ClassAttendance.java
+++
@@ -1,7 +0,0 @@
-package com.akadasoftware.danceworksonline.Classes;
-
-/**
- * Created by Kyle on 7/21/2014.
- */
-public class ClassAttendance {
-}
